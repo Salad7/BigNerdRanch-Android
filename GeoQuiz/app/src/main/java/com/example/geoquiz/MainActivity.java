245dc54e19e439f21fda77934c0cc90a007b28f7
mohamedsalad <kukgiplayer2@gmail.com>
2021-10-24 13:50:10 -0400

    Page 60 update logcat

--- GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.java
+++ GeoQuiz/app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.geoquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     Button mFalseButton;
     Button mNextButton;
     TextView mQuestionTextView;
+    private static final String TAG = "QuizActivity";
     private Question[] mQuestionBank = new Question[]{
             new Question(R.string.question_oceans, true),
             new Question(R.string.question_mideast, true),
@@ -24,8 +26,46 @@ public class MainActivity extends AppCompatActivity {
     int mCurrentIndex = 0;
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart called");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause called");
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume called");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop called");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestroy called");
+
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG,"onCreate(Bundle) called");
+
+
         setContentView(R.layout.activity_main);
 
         mTrueButton = findViewById(R.id.true_button);
