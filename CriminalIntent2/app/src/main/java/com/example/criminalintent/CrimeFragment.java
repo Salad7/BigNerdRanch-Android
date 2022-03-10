d2b275be2e6b7312ed9b5d802654df471fd86eda
mohamedsalad <kukgiplayer2@gmail.com>
2022-03-09 21:08:53 -0500

    225

--- CriminalIntent2/app/src/main/java/com/example/criminalintent/CrimeFragment.java
+++ CriminalIntent2/app/src/main/java/com/example/criminalintent/CrimeFragment.java
@@ -9,11 +9,13 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import java.util.UUID;
 
@@ -22,6 +24,7 @@ public class CrimeFragment extends Fragment {
     private EditText mTitleField;
     private Button mDateButton;
     private CheckBox mSolvedCheckBox;
+    private static final String DIALOG_DATE = "DialogDate";
 
     private static final String ARG_CRIME_ID = "crime_id";
 
@@ -52,7 +55,15 @@ public class CrimeFragment extends Fragment {
 
 
         mDateButton.setText(mCrime.getDate().toString());
-        mDateButton.setEnabled(false);
+//        mDateButton.setEnabled(false);
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getFragmentManager();
+                DatePickerFragment dialog = new DatePickerFragment();
+                dialog.show(fragmentManager, DIALOG_DATE);
+            }
+        });
 
         mSolvedCheckBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
