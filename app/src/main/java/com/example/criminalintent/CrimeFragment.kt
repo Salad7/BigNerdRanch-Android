61b7923c52d061defdefbf5fb47558d5d90bd635
mohamedsalad <kukgiplayer2@gmail.com>
2022-08-18 18:17:52 -0400

    finished chapter on observer/observables

--- app/src/main/java/com/example/criminalintent/CrimeFragment.kt
+++ app/src/main/java/com/example/criminalintent/CrimeFragment.kt
@@ -10,6 +10,8 @@ import androidx.fragment.app.Fragment
 import io.reactivex.rxjava3.annotations.NonNull
 import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.core.Observer
+import io.reactivex.rxjava3.disposables.CompositeDisposable
+import io.reactivex.rxjava3.kotlin.subscribeBy
 
 class CrimeFragment : Fragment() {
 
@@ -22,7 +24,34 @@ class CrimeFragment : Fragment() {
         observable.subscribe{
             Log.d("CrimeFragment",it.toString())
         }
+        val disposables = CompositeDisposable()
+        val createdObservable = Observable.create<Int> { emitter ->
+            emitter.onNext(1)
+            emitter.onNext(2)
+            emitter.onNext(3)
+            emitter.onComplete()
+        }
 
+        val createdSubscriber = createdObservable.subscribeBy(
+                onNext = {
+                    Log.d("CrimeFragment","created Subscriber: "+it.toString())
+                }
+        )
+       val subscription =  observable.subscribeBy(
+                onNext = {
+                    Log.d("CrimeFragment","Item is: "+it.toString())
+                },
+                onError = {
+
+                },
+                onComplete = {
+                    Log.d("CrimeFragment","Completed the syncing")
+                }
+
+        )
+        disposables.add(subscription)
+        disposables.add(createdSubscriber)
+        disposables.dispose()
         return v
     }
 }
